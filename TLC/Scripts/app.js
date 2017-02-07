16e086a423bc0f3f42760ef875e96a6f4d7e3348
micduncan@gmail.com <micduncan@gmail.com>
2017-02-07 18:01:16 -0500

    adding app,js file

---
+++ TLC/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿var app = {
+
+    modalFunction: function (modalTitle, modalContent, modalButton) {
+        var modal = $("#myModal");
+        $(".modal-title").html(modalTitle);
+        modal.find(".modal-body").html(modalContent);
+        $(modalButton).appendTo($("#myModal").find(".modal-footer").empty());
+        modal.show();
+    },
+};
