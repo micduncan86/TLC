3194b063de04691cf69ec72e271faeeb80ae56aa
micduncan@gmail.com <micduncan@gmail.com>
2017-04-15 18:18:08 -0400

    added new files

---
+++ BusinessObjs/CacheManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace TLC.Data
+{
+    public class CacheManager
+    {
+        private Dictionary<string,object> _mylist = null;
+        public Dictionary<string, object> CacheList
+        {
+            get
+            {
+                if (_mylist == null)
+                {
+                    _mylist = (HttpContext.Current.Cache["CacheList"] as Dictionary<string, object>);
+                    if (_mylist == null)
+                    {
+                        _mylist = new Dictionary<string, object>();
+                          HttpContext.Current.Cache.Insert("CacheList", _mylist);
+                    }
+                }
+                return _mylist;
+            }
+            set
+            {
+                HttpContext.Current.Cache.Insert("CacheList", value);
+            }
+        }
+
+        public void Clear()
+        {
+
+            HttpContext.Current.Cache.Remove("CacheList");
+        }
+    }
+}
---
+++ BusinessObjs/ReportParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLC.Data
+{
+    public partial class ReportParameters
+    {
+        #region Properties
+        public string Report { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int TeamId { get; set; }
+        public int MemberId { get; set; }
+
+        #endregion
+
+    }
+}
